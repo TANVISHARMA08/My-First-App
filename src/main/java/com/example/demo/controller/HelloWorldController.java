5ebc64389bc772d956a5b0e438b08a556d771075
TANVISHARMA08 <tanvi8sh@gmail.com>
2021-06-16 22:05:54 +0530

    get by query

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -1,17 +1,23 @@
 package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloWorldController {
-//public static void main(String[] args) {
+
 	@RequestMapping( method = RequestMethod.GET,value = {"/home"})
 	public String sayHello() {
 		return "HELLO WORLD";
+    }
+	
+	@RequestMapping (value = {"/query"} , method = RequestMethod.GET)
+	public String sayHello(@RequestParam(value = "name") String name ) {
+		return "HELLO  " + name + " !! ";
+	}
+	
 }
-}
-
-
-
